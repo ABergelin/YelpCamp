e9ba6a6ec83e0da3ff837c360457f9ec96306cc3
Alex Bergelin <the_bergelin@hotmail.co.uk>
2018-12-03 18:18:05 +0000

    Last commit of 1st YelpCamp lecture

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -15,6 +15,8 @@ app.get("/campgrounds", function(req, res){
             {name: "Storey Arms", image: "http://www.breconbeacons.org/website/var/tmp/thumb_5422__auto_850904660de984af948beee3aee98a4f.jpeg"},
             {name: "Ffrith Beach", image: "https://www.pitchup.com/media.pitchup.co.uk/images/2/image/private/s--1JlwtpfV--/c_fill,h_600,w_800/e_improve,fl_progressive/q_jpegmini/v1487418220/ffrith_beach_touring_park/x218361.jpg.pagespeed.ic.tT-pB1DE-D.jpg"}
         ];
+        
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
